3116f52548ed9d9153d87a0ef456df685b5f6c0d
Destructis <107251998+Destructis@users.noreply.github.com>
2024-08-13 12:25:21 +0200

    patch box info-eleve

--- src/app/info-eleve/info-eleve.component.ts
+++ src/app/info-eleve/info-eleve.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
 
 @Component({
-  selector: 'app-info-eleve',
+  selector: '.app-info-eleve',
   templateUrl: './info-eleve.component.html',
   styleUrl: './info-eleve.component.css'
 })
