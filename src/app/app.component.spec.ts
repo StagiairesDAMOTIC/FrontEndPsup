0610e6010cb62e13ddefd86bc540de8160b88125
Destructis <107251998+Destructis@users.noreply.github.com>
2024-08-13 11:17:10 +0200

    test du component principal

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular_Project_Jaeger'`, () => {
+  it(`should have as title 'FrontEnd_Psup'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular_Project_Jaeger');
+    expect(app.title).toEqual('FrontEnd_Psup');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular_Project_Jaeger app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('FrontEnd_Psup app is running!');
   });
 });
