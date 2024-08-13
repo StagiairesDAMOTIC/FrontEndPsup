de4d2803b32bc8971c74496a3087259f82e3f0ff
Destructis <107251998+Destructis@users.noreply.github.com>
2024-08-13 15:26:14 +0200

    correction du ts

--- src/app/choix-seconde/choix-seconde.component.ts
+++ src/app/choix-seconde/choix-seconde.component.ts
@@ -8,17 +8,3 @@ import { Component } from '@angular/core';
 export class ChoixSecondeComponent {
 
 }
-function openTab(x: number): void {
-  // Sélectionne tous les éléments avec la classe "search-form"
-  const contents = document.querySelectorAll<HTMLElement>(".search-form");
-
-  // Parcourt tous les éléments et les cache
-  for (let i = 0; i < contents.length; i++) {
-    contents[i].style.display = "none";
-  }
-
-  // Affiche l'élément correspondant à l'indice x
-  if (contents[x]) {
-    contents[x].style.display = "block";
-  }
-}
