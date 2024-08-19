74d0fec39040c50b324d3e142da787bc3e6e80c3
Louka <lmorandi56@gmail.com>
2024-08-19 10:09:06 +0200

    zone de recherche dans trouver sa formation

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: '../app.component.css',
 })
 export class ArticleComponent {}
